96e8327acbb49ef50f2386d515488d013a89f16e
stylusjoe-proj <56069928+stylusjoe-proj@users.noreply.github.com>
2019-10-02 05:45:07 -0400

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Helper
+    {
+        public void AddToVals(ref int input1, ref int input2, int increment)
+        {
+            input1 += increment;
+            input2 += increment;
+        }
+
+        public void Swap(ref int input1, ref int input2)
+        {
+            int placeInput1 = input1;
+            int placeInput2 = input2;
+            input1 = placeInput2;
+            input2 = placeInput1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Helper myInstance = new Helper();
+
+                int input1 = 10;
+                int input2 = 20;
+
+                Console.WriteLine("AddToVals...");
+                Console.WriteLine("\tCalling with val1 = {0}, val2 = {1}", input1, input2);
+
+            Console.WriteLine("Input value for AddToVals increment...");
+            int val = Convert.ToInt32(Console.ReadLine());
+            myInstance.AddToVals(ref input1, ref input2, val);
+                Console.WriteLine("After AddToVals has been called...");
+                Console.WriteLine("\tval1 is now {0}, val2 is now = {1}", input1, input2);
+                myInstance.Swap(ref input1, ref input2);
+                Console.WriteLine("After SwapVals has been called...");
+                Console.WriteLine("\tval1 is now {0}, val2 is now = {1}", input1, input2);
+                Console.WriteLine("Press any key to continue...");
+                Console.Read();
+            
+        }
+    }
+}
